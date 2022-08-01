96e3f4ab64bb90e04b5e4c17b983141ab3d6e933
Steven Yung <stevenyung@fastmail.com>
2022-08-01 16:25:59 +0200

    upgrade fresh-session

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -1,2 +1,2 @@
 export * from "../deps/fresh/server.ts";
-export * as session from "https://deno.land/x/fresh_session@0.1.4/mod.ts";
+export * as session from "https://deno.land/x/fresh_session@0.1.5/mod.ts";
