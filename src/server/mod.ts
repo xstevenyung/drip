540c2cfefbf884cefb5ab687f2f4a422eee6b6cc
Steven Yung <stevenyung@fastmail.com>
2022-08-04 14:54:20 +0200

    override Fresh Handlers

--- src/server/mod.ts
+++ src/server/mod.ts
@@ -1,5 +1,6 @@
 import { Manifest, session, start as baseStart, StartOptions } from "./deps.ts";
 import { setup as setupSupabase } from "../supabase/mod.ts";
+import type { Handlers as BaseHandlers } from "./deps.ts";
 
 export type State = session.WithSession;
 
@@ -8,6 +9,8 @@ export function start(routes: Manifest, options: StartOptions = {}) {
 
   return baseStart(routes, options);
 }
+
+export type Handlers<D = any, S = State> = BaseHandlers<D, S>;
 
 export * from "./deps.ts";
 export { session } from "./deps.ts";
