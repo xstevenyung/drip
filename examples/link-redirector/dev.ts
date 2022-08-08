f9a2934fc962e58d70a3c7ead5f84670213b7d85
Steven Yung <stevenyung@fastmail.com>
2022-08-08 15:43:06 +0200

    add dev module system

--- examples/link-redirector/dev.ts
+++ examples/link-redirector/dev.ts
@@ -1,5 +1,11 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
-import { dev } from "drip/dev.ts";
+import { dev, supabase, tailwind } from "drip/dev.ts";
 
-await dev(import.meta.url, "./main.ts");
+await dev(import.meta.url, "./main.ts", [
+  supabase(),
+  tailwind({
+    input: "./styles/main.css",
+    output: "./static/styles/main.css",
+  }),
+]);
--- src/dev/mod.ts
+++ src/dev/mod.ts
@@ -1,10 +1,49 @@
 import { configSync, dev as baseDev } from "./deps.ts";
-// import {
-//   // generateTypes as generateDatabaseTypes,
-//   // startDev as startSupabaseDev,
-// } from "../supabase/mod.ts";
+import {
+  generateTypes as generateDatabaseTypes,
+  startDev as startSupabaseDev,
+} from "../supabase/mod.ts";
 
-export function dev(base: string, entrypoint: string) {
+export function supabase() {
+  return async () => {
+    if (!Deno.env.get("SUPABASE_DEV_STARTED")) {
+      await startSupabaseDev();
+    }
+
+    Deno.env.set("SUPABASE_DEV_STARTED", "true");
+
+    generateDatabaseTypes();
+  };
+}
+
+export type TailwindModuleOptions = { input: string; output: string };
+
+export function tailwind(options: TailwindModuleOptions) {
+  return () => {
+    const cwd = new URL(".", Deno.env.get("CWD")).pathname;
+
+    return Deno.run({
+      cmd: [
+        "npx",
+        "tailwindcss",
+        "-i",
+        options.input,
+        "-o",
+        options.output,
+      ],
+      cwd,
+      stdout: "piped",
+    }).status();
+  };
+}
+
+export type DripDevModule = () => Promise<any>;
+
+export function dev(
+  base: string,
+  entrypoint: string,
+  modules?: DripDevModule[],
+) {
   Deno.env.set("CWD", base);
 
   // copied from https://deno.land/std@0.148.0/dotenv/load.ts
@@ -21,13 +60,8 @@ export function dev(base: string, entrypoint: string) {
     });
   }
 
-  if (!Deno.env.get("SUPABASE_DEV_STARTED")) {
-    // await startSupabaseDev();
-  }
-
-  Deno.env.set("SUPABASE_DEV_STARTED", "true");
-
-  // generateDatabaseTypes();
-
-  return baseDev(base, entrypoint);
+  return Promise.all([
+    baseDev(base, entrypoint),
+    ...(modules ? modules.map((module) => module()) : []),
+  ]);
 }
