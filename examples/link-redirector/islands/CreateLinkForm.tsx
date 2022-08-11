0e4a21f60c4721aaccc56c0847a75652c5b5444e
Steven Yung <stevenyung@fastmail.com>
2022-08-11 11:46:27 +0200

    update link redirector example

--- examples/link-redirector/islands/CreateLinkForm.tsx
+++ examples/link-redirector/islands/CreateLinkForm.tsx
@@ -14,7 +14,36 @@ export default function ({ baseURL }: Props) {
       shape={shape}
       onSuccess={(data) => {
         updateGlobalStore({
-          _success: `Sucessfully created: ${baseURL}/${data.slug}`,
+          _success: (
+            <>
+              Sucessfully created
+
+              <button
+                type="button"
+                class="flex gap-0.5 items-center"
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(`${baseURL}/${data.slug}`);
+                }}
+              >
+                <span class="underline">{baseURL}/{data.slug}</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+                  />
+                </svg>
+              </button>
+            </>
+          ),
         });
       }}
       class="space-y-6"
